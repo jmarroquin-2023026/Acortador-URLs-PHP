bf6d413d91e9e26c9652b1a7417ceef32f1640a7
Josue <josue@MacBook-Pro-de-Josue.local>
2026-01-19 17:31:23 -0600

    feat(ui): mejora el diseño del dashboard y agrega estilos CSS

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,15 +5,33 @@
     <meta charset="UTF-8">
     <title>Acortador de URLs</title>
     <link rel="stylesheet" href="/dashboard.css">
+    <link rel="icon" type="image/png" href="https://www.quickshipping.com/_assets/imgs/logos/logo_quick_shipping_guaranty_check.png">
 </head>
 
 <body>
+    <nav class="navbar">
+        <div class="nav-toggle" onclick="document.querySelector('.nav-list').classList.toggle('active')">
+            <span></span>
+        </div>
+        <ul class="nav-list">
+            <li>
+                <a href="/">
+                    <img class="logo" 
+                    src='https://www.quickshipping.com/_assets/imgs/logos/logo_quick_shipping_logistics.png' alt="quickshipping-logo">
+                </a>
+            </li>
+        </ul>
+    </nav>
     @if(session('success'))  
-        <div>{{session('success')}}</div>
+        <div class="success-message">
+            <span class="text-info">{{session('success')}}</span>
+        </div>
     @endif
 
     @if(session('error'))
-        <div>{{session('error')}}</div>
+        <div class="error-message">
+            <span class="text-info">{{session('error')}}</span>
+        </div>
     @endif
 
     <div class="container">
@@ -25,7 +43,7 @@
             </form>
 
         <h1>Buscar URL por codigo corto</h2>
-        <form id="searchForm" method="GET" style="margin-bottom: 20px;">
+        <form id="searchForm" method="GET" >
             <input type="text" id="searchId" placeholder="Ingresa el ID" required class="input-insert">
             <button type="submit" class="button-action">Buscar</button>
         </form>
@@ -41,54 +59,55 @@
                 window.location.href = `/show/${id}`;
             });
         </script>
-
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>URL Original</th>
-                <th>URL Corta</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($urls as $url)
+    <div class="table-wrapped">
+        <table>
+            <thead>
                 <tr>
-                    <td>{{ $url->id }}</td>
-                    <td>
-                        <form action="{{ url('update/' . $url->id) }}" method="POST" style="display:flex; gap:5px; align-items:center;">
-                            @csrf
-                            @method('PUT')
-                            <input type="url"
-                                name="original_url"
-                                value="{{ $url->original_url }}"
-                                readonly
-                                ondblclick="this.removeAttribute('readonly')"
-                                onblur="this.setAttribute('readonly', true)"
-                                title="Doble click para editar"
-                                class='input-text'
-                                required>
-                            <button class="button-action" type="submit">Actualizar</button>
-                        </form>
-                    </td>
-                    <td>
-                        <form action="{{ url('delete/' . $url->id) }}" method="POST" style="display:inline" class="actions">
-                            <a href="{{ url('api/' . $url->shorten_url) }}" target="_blank" class="link">
-                            {{ url('api/' . $url->shorten_url) }}
-                        </a>
-                            @csrf
-                            @method('DELETE')
-                            <button class="button-action" type="submit"
-                                onclick="return confirm('¿Seguro que quieres eliminar esta URL?')">Eliminar</button>
-                        </form>
-                    </td>
+                    <th>ID</th>
+                    <th>URL Original</th>
+                    <th>URL Corta</th>
                 </tr>
-            @empty
-                <tr>
-                    <td colspan="3">No hay URLs registradas.</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @forelse($urls as $url)
+                    <tr>
+                        <td>{{ $url->id }}</td>
+                        <td>
+                            <form action="{{ url('update/' . $url->id) }}" method="POST" style="display:flex; gap:5px; align-items:center;">
+                                @csrf
+                                @method('PUT')
+                                <input type="url"
+                                    name="original_url"
+                                    value="{{ $url->original_url }}"
+                                    readonly
+                                    ondblclick="this.removeAttribute('readonly')"
+                                    onblur="this.setAttribute('readonly', true)"
+                                    title="Doble click para editar"
+                                    class='input-text'
+                                    required>
+                                <button class="button-action" type="submit">Actualizar</button>
+                            </form>
+                        </td>
+                        <td>
+                            <form action="{{ url('delete/' . $url->id) }}" method="POST" style="display:inline" class="actions">
+                                <a href="{{ url('api/' . $url->shorten_url) }}" target="_blank" class="link">
+                                {{ url('api/' . $url->shorten_url) }}
+                            </a>
+                                @csrf
+                                @method('DELETE')
+                                <button class="button-action" type="submit" onclick="return confirm('¿Seguro que quieres eliminar esta URL?')">Eliminar</button>
+                            </form>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="3">No hay URLs registradas.</td>
+                    </tr>
+                    
+                @endforelse
+            </tbody>
+        </table>
+    </div>
     <div class="pagination">
         {{$urls->links()}}
     </div>
